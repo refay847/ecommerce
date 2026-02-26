559d874a697d038e854af8bafc1eb3c1d80fe5fe
refay <abdelrahman.mohamed2178@gmail.com>
2026-02-26 22:12:40 +0200

    create devise auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,20 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  # allow_browser versions: :modern
 
   # Changes to the importmap will invalidate the etag for HTML responses
-  stale_when_importmap_changes
+  # stale_when_importmap_changes
+
+  # Tell Devise to use the main layout
+  layout :layout_by_resource
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "application"  # <- uses your normal layout
+    else
+      "application"
+    end
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,40 +1,33 @@
 class PagesController < ApplicationController
-  layout false, only: [:home]
-  def home 
+  before_action :authenticate_user!, only: [ :cart, :checkout ]
+  layout false, only: [ :home ]
+  def home
   end
 
-  def about 
+  def about
   end
 
   def shop
-    
   end
 
   def news
-    
   end
 
   def contact
-    
   end
 
   def single_news
-    
   end
 
   def single_product
-    
   end
 
   def cart
-    
   end
 
   def checkout
-    
   end
 
   def error_404
-    
   end
 end
